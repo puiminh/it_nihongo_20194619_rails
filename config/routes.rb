261a2fb3b384cc57730afc3858bda3cda44bdcee
volmicrolab <volmicrolab@gmail.com>
2023-01-10 13:37:39 +0700

    add home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get "/" => "static_pages#home"
+  get "/log_out", to: "static_pages#logOut"
   resources :books
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
